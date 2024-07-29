aa055337a8baa41aeb6e33d50bbff05d79f631e9
Marcus Little <mcolelittle@gmail.com>
2024-07-29 16:09:29 -0400

    add csr logic, remove edge function

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react'
+import ProxyWrapper from './src/components/ProxyWrapper'
+
+export const wrapPageElement = ({ element, props }) => {
+  return <ProxyWrapper {...props}>{element}</ProxyWrapper>
+}
---
+++ src/components/proxy-wrapper.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+
+const ProxyWrapper = ({ children }) => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const path = window.location.pathname
+      if (path.startsWith('/docs-qa/kubernetes-operator/v1.22/')) {
+        fetch(`https://www.marcusfromtroy.com/${path.slice('/docs-qa/kubernetes-operator/v1.22'.length)}`)
+          .then(response => response.text())
+          .then(html => {
+            document.open()
+            document.write(html)
+            document.close()
+          })
+          .catch(error => console.error('Error fetching content:', error))
+      }
+    }
+  }, [])
+
+  return <>{children}</>
+}
+
+export default ProxyWrapper
